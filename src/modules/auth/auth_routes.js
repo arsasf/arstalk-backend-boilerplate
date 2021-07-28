79929c4df89fff48864e6fbd57c50c58a1b397dd
arsasf <auliasafitri2698@gmail.com>
2021-07-28 11:08:49 +0800

    update login

--- src/modules/auth/auth_routes.js
+++ src/modules/auth/auth_routes.js
@@ -1,21 +1,8 @@
 const express = require('express')
 const Route = express.Router()
-const authMiddleware = require('../../middleware/auth')
-const { clearDataUserRedis } = require('../../middleware/redis')
-const {
-  register,
-  login,
-  verify,
-  updateUserOffline
-} = require('./auth_controller')
+const { register, login, verify } = require('./auth_controller')
 
 Route.post('/login', login)
 Route.post('/register', register)
 Route.get('/verify/:hash', verify)
-Route.get(
-  '/logout/status-user/:id',
-  authMiddleware.authentication,
-  clearDataUserRedis,
-  updateUserOffline
-)
 module.exports = Route
